661d6c6aaa21468363e68f0477d0f36da82a532e
RestedWickedDev <jesusdperez@gmail.com>
2020-12-07 23:47:19 -0800

    Finished Quarry

--- quarry.py
+++ quarry.py
@@ -31,47 +31,53 @@ class Crystal(Ore):
         self.value = self.value * random_value
         return self.value
 
-
-class Smeltery:
-    def __init__(self, name, temperature):
-        self.name = name
-        self.temperature = temperature
-
-    def deposit_ore(self):
-        return
-
-    def process_ore(self):
-        for ore in Quarry.ore:
-            Ore.smelt()
-            ore.value + self.temperature
-        return
-
 class Quarry:
     def __init__(self, name, resources):
         self.name = name
         self.resources = resources
 
         self.ore = list()
+        self.crystal = list()
 
-    def obtain_ore(self, Ore):
-        self.ore.append(Ore)
-        return self.ore
+    def obtain_ore(self, name, value):
+        name = Ore("iron", value)
+        self.ore.append(name)
 
+    def obtain_crystal(self, name, value):
+        name = Crystal("diamond", value)
+        self.crystal.append(name)
 
     def sell_all(self):
         sum_value = 0
         for ore in self.ore:
             sum_value += ore.value
+        for crystal in self.crystal:
+            sum_value += crystal.value
         return sum_value
 
+class Smeltery:
+    def __init__(self, name, temperature):
+        self.name = name
+        self.temperature = temperature
+        self.Quarry = Quarry("BlackCrow", "Iron")
+
+    def process_ore(self):
+        for ore in self.Quarry.ore:
+            ore.mine()
+            ore.smelt()
+            value = ore.value + self.temperature
+
+    def process_crystal(self):
+        for crystal in self.Quarry.crystal:
+            crystal.mine()
+            crystal.smelt()
+            value = crystal.value + self.temperature
+
+
 if __name__ == "__main__":
-    ore = Ore("Iron", 0)
-    ore.mine()
-    ore2 = Crystal("Diamond", 0)
-    ore2.mine()
-    smelt = Smeltery("Skyforge", 100)
-    smelt.process_ore()
-    quarry = Quarry("BlackCrow Mine", "iron")
-    quarry.obtain_ore(ore)
-    quarry.obtain_ore(ore2)
-    print(quarry.sell_all())
+    smeltery = Smeltery("SuperForge", 100)
+    smeltery.Quarry.obtain_ore("ore", 0)
+    smeltery.Quarry.obtain_crystal("crystal", 0)
+    smeltery.process_ore()
+    smeltery.process_crystal()
+    print(smeltery.Quarry.sell_all())
